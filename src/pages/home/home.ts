bccacb87ddfd0d935a064b134505fea798ecda28
Connor Dodge <connor.m.dodge@gmail.com>
2017-04-11 17:30:10 -0600

    adding songs works

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  songs: FirebaseListObservable<any>;
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, af: AngularFire) {
+    this.songs = af.database.list('/songs');
   }
+
+  addSong(){
+    let prompt = this.alertCtrl.create({
+      title: 'Song Name',
+      message: "Enter a name for this new song you're so keen on adding",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.songs.push({
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+}
 
 }
